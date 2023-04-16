c7d46838c99e243e6b363bd0f2616b5c43cf60dc
Mustafa Dinçer <mustafa.dincer@ogr.iuc.edu.tr>
2023-04-16 03:08:04 +0300

    Decarator ve Singleton Design Pattern

---
+++ Decorator/Decorator/Program.cs
@@ -0,0 +1,72 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ICar car = new Car();
+        Console.WriteLine(car.Özellikler());
+
+        CarDecorator carDecorator = new Sunroof(car);
+        Console.WriteLine(carDecorator.Özellikler());
+
+        ICar car2 = new Car();
+        CarDecorator carDecorator1 = new Sunroof(new Navigasyon(car));
+        Console.WriteLine(carDecorator1.Özellikler());
+    }
+}
+
+interface ICar
+{
+    string Özellikler();
+    
+}
+
+public class Car : ICar
+{
+    public string Özellikler() {
+
+        return "Klasik Araba Özellikleri";
+    
+    }
+
+}
+
+abstract class CarDecorator : ICar
+{
+    protected ICar car;
+
+    protected CarDecorator(ICar car)
+    {
+        this.car = car;
+    }
+
+
+    public virtual string Özellikler()
+    {
+        return car.Özellikler().ToString(); 
+    }
+}
+
+class Sunroof : CarDecorator
+{
+    public Sunroof(ICar car) : base(car)
+    {
+    }
+
+    public override string Özellikler()
+    {
+        return car.Özellikler() + " Sunroof" ;
+    }
+
+}
+
+class Navigasyon : CarDecorator
+{
+    public Navigasyon(ICar car) : base(car)
+    {
+    }
+
+    public override string Özellikler()
+    {
+        return car.Özellikler() + "  Navigasyon";
+    }
+}
---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+public class DataProcessor
+{
+    public string[] ProcessData(string[] data)
+    {
+        string[] processedData = new string[data.Length];
+
+        for (int i = 0; i < data.Length; i++)
+        {
+            processedData[i] = data[i].ToUpper(); // Converting to uppercase
+        }
+
+        return processedData;
+    }
+}
+
+public class DataHandler
+{
+    public string[] GetData()
+    {
+        // Code for getting data from external source
+        // ...
+        string[] data = { "apple", "banana", "cherry" };
+        return data;
+    }
+
+    public void StoreData(string[] data)
+    {
+        // Code for storing data to external destination
+        // ...
+        Console.WriteLine("Data stored successfully!");
+    }
+}
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        DataProcessor processor = new DataProcessor();
+        DataHandler handler = new DataHandler();
+
+        string[] data = handler.GetData();
+        string[] processedData = processor.ProcessData(data);
+        handler.StoreData(processedData);
+    }
+}
