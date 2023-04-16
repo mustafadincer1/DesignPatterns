be6440f81604f71054dfec017c2b682ce947a2c2
Mustafa Dinçer <mustafa.dincer@ogr.iuc.edu.tr>
2023-04-16 03:06:29 +0300

    Composite ve Command Design Pattern

---
+++ Command/Command/Program.cs
@@ -0,0 +1,79 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Light light = new Light();
+        ICommand _TurnOnCommand = new TurnOnCommand(light);
+        ICommand _TurnOffCommand = new TurnOffCommand(light);
+        CommandManager manager = new CommandManager();
+        manager.SetCommand(_TurnOnCommand);
+        manager.ExecuteCommand();
+        manager.SetCommand(_TurnOffCommand);
+        manager.ExecuteCommand();
+
+    }
+}
+
+public class Light
+{
+    public void TurnOn()
+    {
+        Console.WriteLine("Lamba açıldı.");
+    }
+
+    public void TurnOff()
+    {
+        Console.WriteLine("Lamba kapatıldı.");
+    }
+}
+public interface ICommand
+{
+    public void Execute();
+}
+
+public class TurnOnCommand : ICommand
+{
+    private Light _light;
+
+    public TurnOnCommand(Light light)
+    {
+        _light = light;
+    }
+
+    public void Execute()
+    {   
+        _light.TurnOn();
+    }
+}
+
+public class TurnOffCommand : ICommand
+{
+    private Light _light;
+
+    public TurnOffCommand(Light light)
+    {
+        _light = light;
+    }
+
+    public void Execute()
+    {
+        _light.TurnOff();
+    }
+}
+
+public class CommandManager
+{
+    private ICommand _command;
+
+    public void SetCommand(ICommand command)
+    {
+        _command = command;
+    }
+
+    public void ExecuteCommand()
+    {
+        _command.Execute();
+    }
+
+
+}
---
+++ Composite/Composite/Program.cs
@@ -0,0 +1,62 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        IComponent leaf = new Leaf();
+        IComponent leaf1 = new Leaf();
+        IComponent leaf2 = new Leaf();
+        Composite composite = new Composite();
+
+       
+        composite.add(leaf);
+        composite.add(leaf1);
+        composite.add(leaf2);
+        composite.oparation();
+    }
+}
+
+
+interface IComponent
+{
+    public void oparation();
+}
+
+class Leaf : IComponent
+{
+    public void oparation()
+    {
+        Console.WriteLine("Leaf is runnin");
+    }
+}
+
+class Composite : IComponent
+{
+    private List<IComponent> children = new List<IComponent>();
+
+    public void add(IComponent component)
+    {
+
+        children.Add(component);
+    }
+
+    public void remove(IComponent component)
+    {
+        children.Remove(component);
+    }
+    public IComponent getComponent(int index)
+    {
+        return children[index];
+    }
+    public void oparation()
+    {
+        Console.WriteLine("Composite is running");
+
+        foreach (IComponent component in children)
+        {
+            component.oparation();
+            
+        }
+    }
+}
+
