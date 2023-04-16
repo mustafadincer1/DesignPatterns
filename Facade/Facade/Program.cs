b5e79a88a6f917db8335c16b65e117bc816c9b03
Mustafa Dinçer <mustafa.dincer@ogr.iuc.edu.tr>
2023-04-16 03:09:09 +0300

    Facade ve Factory Design Pattern

---
+++ Facade/Facade/Program.cs
@@ -0,0 +1,44 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Facade facade = new Facade();
+        facade.Operation();
+    }
+}
+
+class SubSystem1 { 
+    
+    public void Method1()
+    {
+        Console.WriteLine("Method1 is running");
+    }
+
+}
+class SubSystem2
+{
+    public void Method2()
+    {
+        Console.WriteLine("Method2 is running");
+    }
+}
+
+class Facade
+{   
+
+    private SubSystem1 SubSystem1 { get; set; }
+    private SubSystem2 SubSystem2 { get; set; }
+
+    public Facade()
+    {
+        SubSystem1 = new SubSystem1();
+        SubSystem2 = new SubSystem2();
+    }
+    public void Operation()
+    {
+        SubSystem1.Method1();
+        SubSystem2.Method2();
+    }
+    
+}
+
---
+++ Factory/Factory/Program.cs
@@ -0,0 +1,86 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var x = new Client();
+        x.main();
+    }
+}
+
+class Client
+{
+
+    public void main()
+    {
+        Console.WriteLine("ConcreteCreater1 is running");
+        ClientCode(new ConcreateCreator1());
+
+        Console.WriteLine("\n \n");
+
+        Console.WriteLine("ConcreteCreater2 is running");
+        ClientCode(new ConcreateCreator2());
+    }
+
+    void ClientCode(Creator creator)
+    {
+        Console.WriteLine("Client: I'm not aware of the creator's class," +
+                "but it still works.\n" + creator.SomeOperation());
+    }
+
+
+    public interface IProduct
+    {
+        string operation();
+
+    }
+    public class ConcreateProduct1 : IProduct
+    {
+        public string operation()
+        {
+            return "Result of ConcreateProduct1.";
+        }
+    }
+
+    public class ConcreateProduct2 : IProduct
+    {
+        public string operation()
+        {
+            return "Result of ConcreateProduct2.";
+        }
+    }
+
+
+
+
+    abstract class Creator
+    {
+
+        public abstract IProduct FactoryMethod();
+
+
+        public string SomeOperation()
+        {
+
+            var product = FactoryMethod();
+
+            return product.operation();
+        }
+    }
+
+    class ConcreateCreator1 : Creator
+    {
+        public override IProduct FactoryMethod()
+        {
+            return new ConcreateProduct1();
+        }
+    }
+
+    class ConcreateCreator2 : Creator
+    {
+        public override IProduct FactoryMethod()
+        {
+            return new ConcreateProduct2();
+        }
+    }
+}
+
