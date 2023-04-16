420d8502527f842f267db2bfb87feda12a9b9a7a
Mustafa Dinçer <mustafa.dincer@ogr.iuc.edu.tr>
2023-04-16 03:11:14 +0300

    Prototype ve Proxy design pattern

---
+++ Prototype/Prototype/Program.cs
@@ -0,0 +1,68 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Person person1 = new Person();
+        person1.age = 21;
+        person1.birthday = Convert.ToDateTime("16.11.2002");
+        person1.Name = "Mustafa";
+        person1.idInfo = new IdInfo(600);
+        DisplayValues(person1 );
+        Person person2 = person1.ShallowCopy();
+        Person person3 = person1.DeepCopy();
+
+        DisplayValues(person2 );
+        DisplayValues(person3);
+
+        person1.age = 30;
+        person1.idInfo.IdNumber = 1;
+        DisplayValues(person1);
+        DisplayValues(person2);
+        DisplayValues(person3);
+
+
+
+    }
+    public static void DisplayValues(Person p)
+    {
+        Console.WriteLine("      Name: {0:s}, Age: {1:d}, BirthDate: {2:MM/dd/yy}",
+            p.Name, p.age, p.birthday);
+        Console.WriteLine("      ID#: {0:d}", p.idInfo.IdNumber);
+    }
+}
+
+
+public class Person
+{
+
+    
+
+    public int age { get; set; }
+    public string Name { get; set; }
+    public DateTime birthday { get; set; }
+    public IdInfo  idInfo { get; set; }
+
+    public Person ShallowCopy() { 
+        return (Person) this.MemberwiseClone();
+        
+    }
+
+    public Person DeepCopy() {
+        Person clone = (Person) this.MemberwiseClone();
+        clone.idInfo =  new IdInfo(idInfo.IdNumber);
+        clone.Name = String.Copy(Name);
+        return clone;
+    }
+}
+
+public class IdInfo { 
+
+    public int IdNumber { get; set; }
+
+    public IdInfo(int ıdNumber)
+    {
+        IdNumber = ıdNumber;
+    }
+
+    
+}
---
+++ Proxy/Proxy/Program.cs
@@ -0,0 +1,65 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        IImage image = new ProxyImage("test");
+        image.Display();
+        image.Display();
+    }
+}
+
+interface IImage
+{
+    void Display();
+    
+}
+
+class RealImage : IImage
+{
+    private string _filename;
+
+    public RealImage(string filename)
+    {
+        _filename = filename;
+        LoadFromDisk(_filename);
+    }
+
+    private void LoadFromDisk(string fileName)
+    {
+        Console.WriteLine("Loading " + fileName);
+    }
+
+    public void Display()
+    {
+        Console.WriteLine("Displaying " + _filename); 
+    }
+
+
+}
+
+
+class ProxyImage : IImage
+{
+    private RealImage _image;
+    private string _filename;
+
+    public ProxyImage(string filename)
+    {
+        _filename=filename;
+
+       
+    }
+    public void Display()
+    {
+       if (_image == null)
+        {
+            _image = new RealImage(_filename);
+        
+
+        }
+       else
+        {
+            _image.Display();
+        }
+    }
+}
